06422ec8a6ee12167a4e86fdb6d929bba0c983b7
Eran <michaelieran@gmail.com>
2025-04-10 21:54:11 +0300

    Add extensions

---
+++ Utils/IQueryableExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace BuffBuddyAPI;
+
+public static class IQueryableExtensions
+{
+    public static IQueryable<T> Paginate<T>(this IQueryable<T> queryable, PaginationDTO pagination)
+    {
+        return queryable
+        .Skip((pagination.Page - 1) * pagination.RecordsPerPage)
+        .Take(pagination.RecordsPerPage);
+
+    }
+}
